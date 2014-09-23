952ba547d9e5755d7d52cdbd15d4101e24047653
alexriabtsev <alexriabtsev@users.noreply.github.com>
2014-09-23 15:42:23 +0300

    Update content.php

--- templates/content.php
+++ templates/content.php
@@ -1,29 +1,6 @@
 <?php
 
-global $ss_framework; 
-<div class="row bigrow">
-  <div class="col-sm-12 news-preview">
-    <?php 
-      $args = array(
-        'post__not_in' => $do_not_duplicate,
-        'posts_per_page' =>4, 
-        'category_name' => 'novini',
-      );
-      query_posts( $args );
-      while ( have_posts() ) : the_post();?>
-      
-        <div class="col-sm-3 news-item"><?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID, 'thumbnail') ); ?>
-<img src="<?php echo $url ?>" class="mainpage" /><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><br><span class="el-icon-calendar icon date"><?php the_time('d.m.Y'); ?></span></div>
-      
-        
-      <?php endwhile;
-
-        wp_reset_query();
-    ?>
-  </div>
-
-</div> 
-<div class="row bigrow">
+global $ss_framework;
 
 echo '<article '; post_class(); echo '>';
 
@@ -52,4 +29,4 @@ echo '<article '; post_class(); echo '>';
 	do_action( 'shoestrap_in_article_bottom' );
 
 echo '</article>';
-</div>
+?>
