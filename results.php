323a82e1a9af10646cb25cb3d82372f812c995ee
alexriabtsev <alexriabtsev@users.noreply.github.com>
2014-10-08 13:29:49 +0300

    Update results.php

--- results.php
+++ results.php
@@ -1,25 +1,13 @@
 <?php
 /*
-Template Name: результаты	
+Template Name: Результати виборів
 */
 
 if ( !has_action( 'shoestrap_page_header_override' ) )
   get_template_part('templates/page', 'header');
 else
   do_action( 'shoestrap_page_header_override' );
-
-if ( !has_action( 'shoestrap_content_page_override' ) )
-  get_template_part('templates/faces');
-else
-  do_action( 'shoestrap_content_page_override' );
-  
-  
-  
-$show_fb_like=false; 
-
-
 ?>
- 
 
 
 <style>
